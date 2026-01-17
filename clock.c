70c48291ee6e926dbbeba853b952f7b84885888b
Itsmanikandan73 <soulquiet8@gmail.com>
2026-01-17 22:07:32 +0530

    file added

---
+++ clock.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+
+	time_t current_time = 0;
+	struct tm *pTime = NULL;
+	bool super_fast = true;
+
+
+	printf("DIGITAL CLOCK\n");
+
+	while(super_fast){
+
+	time(&current_time);
+	
+	pTime = localtime(&current_time);
+	
+
+	printf("%02d:%02d:%02d\n", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);  // we can use pTime pointer like (*pTime).tm_hour
+			
+
+	sleep(1);
+
+	}
+
+	return 0;
+}
