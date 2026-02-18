b287b56c226c2d2bec6ba796a463bb5c11bc3352
Itsmanikandan73 <soulquiet8@gmail.com>
2026-02-18 10:11:33 +0530

    added fprintf() functions

--- clock.c
+++ clock.c
@@ -7,22 +7,19 @@ int main(){
 
 	time_t current_time = 0;
 	struct tm *pTime = NULL;
-	bool super_fast = true;
+	bool condition = true;
 
 
-	printf("DIGITAL CLOCK\n");
+	fprintf(stdout,"DIGITAL CLOCK\n");
 
-	while(super_fast){
-
-	time(&current_time);
+	while(condition){
 	
+	time(&current_time);
 	pTime = localtime(&current_time);
 	
-
-	printf("\rCurrent Time: %02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);  // we can use pTime pointer like (*pTime).tm_hour
+	fprintf(stdout,"\rCurrent Time: %02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);  // we can use pTime pointer like (*pTime).tm_hour
 			
 	fflush(stdout);
-
 	sleep(1);
 
 	}
